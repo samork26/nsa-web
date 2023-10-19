0be9eb0ab53277c520e761fb90d2b728f35dd22c
samork26 <109486611+samork26@users.noreply.github.com>
2023-10-18 23:40:14 -0400

    hamburger menu works and new header with logo
    new header is the logo and hamburger menu works with javascript

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Select the hamburger element and menu
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
 
 // Add an event listener to the hamburger
 hamburger.addEventListener('click', () => {
-    // Toggle the 'active' class on the menu whenever the hamburger is clicked
-    menu.classList.toggle('active');
-});
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}))
+
