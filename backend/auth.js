ee955b770f715995167fcd6d584453aebb5d4525
samork26 <vohra207@gmail.com>
2024-07-28 11:49:37 -0400

    Update auth.js
    changing path to see if this works

--- backend/auth.js
+++ backend/auth.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: "./backend/.env"});
+require('dotenv').config({path: "backend/.env"});
 const {google} = require('googleapis');
 const {exec} = require('child_process');
 
